4002adc1caa2e23a669582816806569b2a21bf65
Prabhanjan Bhat <117516541+prabhanjanak@users.noreply.github.com>
2025-10-31 02:15:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/prabhanjanak-portfolio/', // 👈 EXACTLY this
-  plugins: [react()],
-});
+  base: '/prabhanjanak-portfolio/',
+  plugins: [react()]
+})
